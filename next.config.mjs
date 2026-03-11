11be505173c6956f4d1342df85f6178013fde8de
dte-84 <dte.solutions.llc@gmail.com>
2026-03-11 15:43:47 -0500

    Remove behavioral preview section

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
+  basePath: '/DTE-E-Portfolio',
   trailingSlash: true,
   images: {
     unoptimized: true,
